d2f499d1ffe1ebc09d76cfa8cf17eb4ba9520579
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-23 22:21:48 +0700

    redirect user on publish n cancel

--- vm2/src/pages/PageThreadCreate.vue
+++ vm2/src/pages/PageThreadCreate.vue
@@ -15,7 +15,7 @@
       </div>
 
       <div class="btn-group">
-        <button class="btn btn-ghost">Cancel</button>
+        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
         <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
       </div>
     </form>
@@ -30,23 +30,32 @@
           required: true
         }
       },
+
       data () {
         return {
           title: '',
           text: ''
         }
       },
+
       methods: {
         save () {
           this.$store.dispatch('createThread', {
             forumId: this.forum['.key'],
             title: this.title,
             text: this.text
+          }).then(thread => {
+            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
           })
+        },
+
+        cancel () {
+          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
         }
       }
     }
 </script>
 
 <style scoped>
+
 </style>
