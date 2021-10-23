2f0d58db0fe21e996c31d94102c22d94d6e0041f
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-23 17:09:37 +0700

    edit user's profile

--- vm2/src/components/ForumListItem.vue
+++ vm2/src/components/ForumListItem.vue
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+    import {countObjectProperties} from '@/utils'
+
     export default {
       props: {
         forum: {
@@ -31,9 +33,7 @@
       },
       computed: {
         threadsCount () {
-          return this.forum.threads
-            ? Object.values(this.forum.threads).length
-            : 0
+          return countObjectProperties(this.forum.threads)
         }
       }
     }
