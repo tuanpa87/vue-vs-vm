1a89ff5d4e1662ce0ae1a8c2adb8b2992ac0770e
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-24 15:32:50 +0700

    install firebase

--- vm2/src/main.js
+++ vm2/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -9,6 +10,18 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyAI3xPaupi8hcdyiZFN9pP_z340Uzfy6DA',
+  authDomain: 'playground-fln8563.firebaseapp.com',
+  databaseURL: 'https://playground-fln8563-default-rtdb.firebaseio.com',
+  projectId: 'playground-fln8563',
+  storageBucket: 'playground-fln8563.appspot.com',
+  messagingSenderId: '693158375683',
+  appId: '1:693158375683:web:df0478bb4b135030d33936'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
