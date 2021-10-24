522dd61cf1d92337a31a1ea39f40cd7f5e991af9
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-24 16:38:29 +0700

    fetch categories and forums for the home page

--- vm2/src/store/index.js
+++ vm2/src/store/index.js
@@ -115,6 +115,28 @@ export default new Vuex.Store({
       return dispatch('fetchItem', {resource: 'posts', id, emoji: '💬'})
     },
 
+    fetchForums ({dispatch}, {ids}) {
+      return dispatch('fetchItems', {resource: 'forums', emoji: '🌧', ids})
+    },
+
+    fetchPosts ({dispatch}, {ids}) {
+      return dispatch('fetchItems', {resource: 'posts', emoji: '💬', ids})
+    },
+
+    fetchAllCategories ({state, commit}) {
+      console.log('🔥', '🏷', 'all')
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('vue-forum/categories').once('value', snapshot => {
+          const categoriesObject = snapshot.val()
+          Object.keys(categoriesObject).forEach(categoryId => {
+            const category = categoriesObject[categoryId]
+            commit('setItem', {resource: 'categories', id: categoryId, item: category})
+          })
+          resolve(Object.values(state.categories))
+        })
+      })
+    },
+
     fetchItem ({state, commit}, {id, emoji, resource}) {
       console.log('🔥‍', emoji, id)
       return new Promise((resolve, reject) => {
@@ -123,6 +145,10 @@ export default new Vuex.Store({
           resolve(state[resource][id])
         })
       })
+    },
+
+    fetchItems ({dispatch}, {ids, resource, emoji}) {
+      return Promise.all(ids.map(id => dispatch('fetchItem', {id, resource, emoji})))
     }
   },
 
