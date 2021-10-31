e72bc7afa7306c84dd481e3d698777f4702d7a31
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-31 16:01:07 +0700

    install dotenv

--- vm2/config/prod.env.js
+++ vm2/config/prod.env.js
@@ -1,11 +1,13 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_API_KEY: "'AIzaSyAI3xPaupi8hcdyiZFN9pP_z340Uzfy6DA'",
-  FIREBASE_AUTH_DOMAIN: "'playground-fln8563.firebaseapp.com'",
-  FIREBASE_DATABASE_URL: "'https://playground-fln8563-default-rtdb.firebaseio.com'",
-  FIREBASE_PROJECT_ID: "'playground-fln8563'",
-  FIREBASE_STORAGE_BUCKET: "'playground-fln8563.appspot.com'",
-  FIREBASE_MESSAGING_ID: "'693158375683'",
-  FIREBASE_APP_ID: "'1:693158375683:web:df0478bb4b135030d33936'"
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID),
+  FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID)
 }
