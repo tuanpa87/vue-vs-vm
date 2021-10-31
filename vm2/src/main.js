cc575c390159ef477627b5370bf8decdc6e37f10
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-31 22:02:37 +0700

    sign users in after registering

--- vm2/src/main.js
+++ vm2/src/main.js
@@ -23,6 +23,12 @@ const config = {
 }
 firebase.initializeApp(config)
 
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    store.dispatch('fetchAuthUser')
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
