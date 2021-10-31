413b2d42c18193b20503b189419c710ab50911a3
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-31 23:42:17 +0700

    integrate firebase authentication in navigation guards

--- vm2/src/router/index.js
+++ vm2/src/router/index.js
@@ -13,7 +13,7 @@ import SignIn from '@/pages/PageSignIn'
 import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -55,13 +55,7 @@ export default new Router({
       name: 'Profile',
       component: Profile,
       props: true,
-      beforeEnter (to, from, next) {
-        if (store.state.authId) {
-          next()
-        } else {
-          next({name: 'Home'})
-        }
-      }
+      meta: { requiresAuth: true }
     },
     {
       path: '/me/edit',
@@ -95,3 +89,23 @@ export default new Router({
   ],
   mode: 'history'
 })
+
+router.beforeEach((to, from, next) => {
+  console.log(`🚦 navigating to ${to.name} from ${from.name}`)
+
+  store.dispatch('initAuthentication')
+    .then(user => {
+      if (to.matched.some(route => route.meta.requiresAuth)) {
+        // protected route
+        if (user) {
+          next()
+        } else {
+          next({name: 'Home'})
+        }
+      } else {
+        next()
+      }
+    })
+})
+
+export default router
