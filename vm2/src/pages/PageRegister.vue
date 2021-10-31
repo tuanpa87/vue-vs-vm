b72a43478e782ab6df84e7323313d370f8c62838
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-10-31 16:28:56 +0700

    store users in database

--- vm2/src/pages/PageRegister.vue
+++ vm2/src/pages/PageRegister.vue
@@ -57,7 +57,8 @@
       },
       methods: {
         register () {
-          console.log(this.form)
+          this.$store.dispatch('createUser', this.form)
+            .then(() => this.$router.push('/'))
         }
       },
       created () {
